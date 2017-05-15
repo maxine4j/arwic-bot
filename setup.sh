c1d0db179b8f2f4a66b6cfdc62ea62679939f33b
Arwic <timothyjings@gmail.com>
2017-05-16 06:17:28 +0800

    Updates setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get install ffmpeg
-pip3.5 install discord.py  youtube-dl
+apt-get install ffmpeg libffi-dev libssl-dev python-dev
+pip3.5 install discord.py youtube-dl pafy pynacl
